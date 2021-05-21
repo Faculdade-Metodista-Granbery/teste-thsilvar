bdf203fd3da456820856a27e944057678962546d
Thiago Silva <thsilvar@gmail.com>
2021-05-20 21:13:38 -0300

    TESTE
    TESTE

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <CardQuote 
-        quote='Não caia no golpe TESTE !'
+        quote='Não caia no golpe TESTE OI OI !'
       />
     </View>
   );
